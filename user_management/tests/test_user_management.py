784f0f4aa5a1270e32c38f1765861afaabfe5242
Ingo Fischer <mail@ingofischer.de>
2020-02-26 21:03:28 +0100

    Add example how to run pytest-django with class fixtures

--- user_management/tests/test_user_management.py
+++ user_management/tests/test_user_management.py
@@ -6,25 +6,27 @@ from rest_framework.test import APIClient
 
 
 @pytest.fixture(scope="class")
-def client():
-    user = User.objects.create_user(username="testuser", email="testuser@test.com")
-    user.save()
-    client = APIClient()
-    client.force_authenticate(user)
-    return client
+def client(django_db_setup, django_db_blocker, request):
+    with django_db_blocker.unblock():
+        user = User.objects.create_user(username="testuser", email="testuser@test.com")
+        user.save()
+        client = APIClient()
+        client.force_authenticate(user)
+        request.cls.client = client
 
 
+@pytest.mark.usefixtures("client")
 class TestUserManagement:
     @pytest.mark.django_db
-    def test_import_files_to_db(self, client):
+    def test_import_files_to_db(self):
         url = reverse(viewname="users")
-        response = client.get(url)
+        response = self.client.get(url)
         assert response.status_code == 200
 
     @pytest.mark.django_db
-    def test_add_user(self, client):
+    def test_add_user(self):
         url = reverse(viewname="users")
-        response = client.post(
+        response = self.client.post(
             url, data={"username": "testuser", "email": "foo@bar.de"}
         )
         assert response.status_code == 200
